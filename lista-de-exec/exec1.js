c2714d719eb901178afce86830324e86f1be1262
Deyvisson Eduardo <deyvissoneduardo22@gmail.com>
2020-02-09 01:04:59 -0300

    Lista de exec.

---
+++ lista-de-exec/exec1.js
@@ -0,0 +1,8 @@
+// Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+//multiplicação e divisão desses valores.
+
+function values(value1, value2){
+    console.log(value1+value2, value1-value2, value1*value2, value1/value2 );
+}
+
+values(2, 2);
