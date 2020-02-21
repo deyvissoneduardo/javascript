52cb26ae4953c4c682e74659b31a82ffa55b23d1
Deyvisson Eduardo <deyvissoneduardo22@gmail.com>
2020-02-21 00:56:24 -0300

    Orientacao a Objetos

---
+++ js-OO/ex_funcao.js
@@ -0,0 +1,9 @@
+const pessoa =  {
+    nome: 'Lele',
+    idade: '10',
+    peso: '45'
+}
+
+console.log(Object.keys(pessoa))//somente as chaves
+console.log(Object.values(pessoa))//somente valores
+console.log(Object.entries(pessoa))//chave e valor
---
+++ js-OO/get_set.js
@@ -0,0 +1,13 @@
+const sequecia = {
+    _valor: 1,
+    get valor(){
+        return this._valor++
+    },
+    set valor(valor){
+        if(valor > this._valor){
+            this._valor = valor
+        }
+    }
+}
+
+console.log(sequecia.valor, sequecia.valor)
