12b9069dc2058e2c261deddb4566ad7480cea279
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-02-28 00:23:09 -0300

    global parametros - module

---
+++ node/global-node/clienteGlobal.js
@@ -0,0 +1,7 @@
+require('../global-node/global')
+
+console.log(MyApp.saudacao)
+
+console.log(MyApp.nome)
+MyApp.nome = 'Mudou aqui'
+console.log(MyApp.nome)
---
+++ node/paramentros-modulos/recebendoParametros.js
@@ -0,0 +1,2 @@
+const saudacao = require('../paramentros-modulos/passandoParametros')('nome1', 'nome2', 'nome3')
+console.log(saudacao)
