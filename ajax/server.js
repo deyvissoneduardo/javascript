1482b6cc894d93c93142a86915f3e04c4edea8a1
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-03-11 00:49:02 -0300

    upload com ajax

--- ajax/server.js
+++ ajax/server.js
@@ -6,4 +6,26 @@ app.use(express.static('.'))
 app.use(bodyParse.urlencoded({ extended : true }))
 app.use(bodyParse.json())
 
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback){
+        callback(null, './upload')
+    },
+    filename: function(req, file, callback){
+        callback(null, `${Date.now}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err){
+            return res.end('Ocorreu um error')
+        }
+        res.end('Concluido com sucesso!!!')
+    })
+})
+
 app.listen(8080, () => console.log('Executando.....'))
