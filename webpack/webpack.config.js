d9883f7666301ec7987324fc0f37aac1574562ce
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-04-03 00:48:43 -0300

    webpack-css

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,26 @@
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode: 'development',
-    entry: './src/principal.js'
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public' 
+    },
+    plugin: [
+        new MiniCssExtractPlugin({
+            filename: 'estilo.css'
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+               // 'style-loader',
+                'css-loader', // interpreta o @import
+            ]
+        }]
+    }
 }
