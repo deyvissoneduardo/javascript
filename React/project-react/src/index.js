ee721ebc07d02a83173494102049e2daf58c7782
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-04-24 01:00:55 -0300

    componente com mutiplos parametro

--- React/project-react/src/index.js
+++ React/project-react/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 //import Primary from './components/Primary'
-import Sencond from './components/Second'
+//import Sencond from './components/Second'
+import { PrimaryComponent, SecondComponent } from './components/Multiple'
 
-ReactDOM.render(<Sencond nome="Paramentro" />, document.getElementById('root'))
+ReactDOM.render(
+    <div>
+        <PrimaryComponent nome="Componente 1" />
+        <SecondComponent nome="Componente 2" />
+    </div>
+    , document.getElementById('root'))
