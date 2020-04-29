ceee7df116be15251abace2625331dcc2a49684c
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-04-29 17:21:15 -0300

    implementando rotas com react e componente usuario

--- projeto-Cadastro-de-usuario/frontend/src/components/user/UserCrud.jsx
+++ projeto-Cadastro-de-usuario/frontend/src/components/user/UserCrud.jsx
@@ -22,9 +22,26 @@ export default class UserCrud extends Component {
     state = {...initialState}
 
     claar() {
-
+        this.setState({ user:initialState.user })
     }
-    
+
+    save(){
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post'
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+        axios[method](url, user) 
+            .then(resp => {
+                const list = this.getUpdateList(resp.data)
+                this.setState({ user: initialState, list})
+            })
+    }
+
+    getUpdateList(user){
+        const list = this.state.list.filter(user => user.id !== user.id)
+        list.unshift(user)
+        return list
+    }
+
     render(){
         return (
             <Main {...headerProps}>
