329b9b3608ebbc591ea2d8c3c0236081b3c88274
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-04-30 00:30:09 -0300

    adc responsividade

--- projeto-Cadastro-de-usuario/frontend/src/components/user/UserCrud.jsx
+++ projeto-Cadastro-de-usuario/frontend/src/components/user/UserCrud.jsx
@@ -58,22 +58,14 @@ export default class UserCrud extends Component {
                     <div className="col-12 col-md-6">
                         <div className="form-group">
                             <label>Nome</label>
-                            <input type="text" className="form-control"
-                                name="name"
-                                value={this.state.user.name}
-                                onChange={e => this.updateField(e)}
-                                placeholder="Digite o nome..." />
+                            <input type="text" className="form-control"name="name" value={this.state.user.name} onChange={e => this.updateField(e)} placeholder="Digite o nome..." />
                         </div>
                     </div>
 
                     <div className="col-12 col-md-6">
                         <div className="form-group">
                             <label>E-mail</label>
-                            <input type="text" className="form-control"
-                                name="email"
-                                value={this.state.user.email}
-                                onChange={e => this.updateField(e)}
-                                placeholder="Digite o e-mail..." />
+                            <input type="text" className="form-control" name="email" value={this.state.user.email} onChange={e => this.updateField(e)} placeholder="Digite o e-mail..." />
                         </div>
                     </div>
                 </div>
@@ -81,13 +73,11 @@ export default class UserCrud extends Component {
                 <hr />
                 <div className="row">
                     <div className="col-12 d-flex justify-content-end">
-                        <button className="btn btn-primary"
-                            onClick={e => this.save(e)}>
+                        <button className="btn btn-primary" onClick={e => this.save(e)}>
                             Salvar
                         </button>
 
-                        <button className="btn btn-secondary ml-2"
-                            onClick={e => this.clear(e)}>
+                        <button className="btn btn-secondary ml-2" onClick={e => this.clear(e)}>
                             Cancelar
                         </button>
                     </div>
