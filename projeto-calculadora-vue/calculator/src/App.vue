2f91242a070780eec0884e9b2d6edb8c21152390
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-05-04 23:49:12 -0300

    componentes do projeto

--- projeto-calculadora-vue/calculator/src/App.vue
+++ projeto-calculadora-vue/calculator/src/App.vue
@@ -1,12 +1,14 @@
 <template>  
     <div id="app">
-        <h1>Ola Vue!</h1>
+        <h1>Calculadora com Vue</h1>
+        <Calculator />
     </div>
 </template>
 
 <script>
+import Calculator from './main/Calculator'
 export default {
-    
+    components: { Calculator }    
 }
 </script>
 
